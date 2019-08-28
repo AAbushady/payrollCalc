57457eba9ee044ff7d6a21d6972f40f1c3ecc100
Alex Abushady <aabushady214@gmail.com>
2019-08-28 14:45:52 -0400

    Update payroll_calc.js
    Added Problem Statement

---
+++ payroll_calc.js
@@ -0,0 +1,14 @@
+/*
+    You have been asked to create a program called "payroll_calc.js", that computes and displays
+    paycheck information for employees. The program should compute the gross pay. After it has computed
+    the gross pay, it should deduct three types of income tax witholding - federal (at a rate of 15%),
+    state (at 3.4%), and county (at 1.1%) - as well as the amount of the employee's medical insurace deduction.
+    Finally, the program should total the deductions and calculate the net pay, and display ALL this
+    information, including the employe's name. Use the following values:
+    
+    First Name: Meredith
+    Last Name: Fitch
+    Hours Worked: 40
+    Hourly Wage Rate: $15.50
+    Medical Insurance Deduction: $55.00
+*/
